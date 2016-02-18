69ee5d54272a945ed5c5e3f015a0a413de226080
Nathan Killen <nathan@theprogrampdx.com>
2016-02-18 14:09:36 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Icon Library Skeleton #
+* Author: Nathan Killen
+* This is a starter pack for a custom icon library.
+* Version 0.1.0
---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    sassLint = require('gulp-sass-lint'),
+    sourcemaps = require('gulp-sourcemaps'),
+    del = require('del');
+
+/**
+ * Style Task
+ */
+gulp.task('styles', function() {
+    return sass('scss/fonts.scss', { style: 'expanded' })
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(gulp.dest('css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(cssnano())
+        .pipe(gulp.dest('css'))
+        .pipe(sassLint())
+        //.pipe(sassLint.format())
+        .pipe(sassLint.failOnError())
+        .pipe(notify({ message: 'Styles task complete' }));
+});
+
+
+/**
+ * Run Before Production
+ */
+gulp.task('clean', function() {
+    return del(['css']);
+});
+
+/**
+ * Watch Task to watch Styles, scripts, and images
+ **/
+gulp.task('watch', function() {
+    gulp.watch('scss/*.scss', ['styles']);
+});
+
+
+/**
+ * Gulp Task
+ */
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles');
+});
